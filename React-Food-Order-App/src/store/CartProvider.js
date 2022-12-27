ed58d86f6605a8a9dad5e580d3d89dec58e418af
Deekshith Y <112107483+DeekshithY@users.noreply.github.com>
2022-12-27 18:18:58 +0530

    commited

---
+++ React-Food-Order-App/src/store/CartProvider.js
@@ -0,0 +1,46 @@
+import { useReducer } from 'react';
+import CartContext from './cart-context';
+
+
+const defaultCartState ={
+    item:[],
+    totalAmount:0   
+};
+
+const cartReducer= (state,action)=>{
+    if(action.type==='ADD'){
+            const UpdateItems =state.item.concat(action.item);
+            const UpdateTotalAmount = state.totalAmount +action.item.price *action.item.price;
+            return{
+                item :UpdateItems,
+                totalAmount:UpdateTotalAmount
+            }
+    }
+    return defaultCartState;
+}
+
+const CartProvider = (props) => {
+
+     const [cartState ,dispatchCartAction]  = useReducer(cartReducer,defaultCartState);
+
+	const addItemToCartHandler = (item) => {
+        dispatchCartAction({type:'ADD',item:item});
+    };
+	const removeItemToCartHandler = (id) => {
+        dispatchCartAction({type:'REMOVE',id:id})
+    };
+
+	const cartContext = {
+		item: cartState.item,
+		totalAmount: cartState.totalAmount,
+		addItem: addItemToCartHandler,
+		removeItem: removeItemToCartHandler,
+	};
+
+	return (
+		<CartContext.Provider value={cartContext}>
+			{props.children}
+		</CartContext.Provider>
+	);
+};
+export default CartProvider;
